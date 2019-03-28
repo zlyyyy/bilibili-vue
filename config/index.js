b9234734d2561c2ab1579ea72e640bbf19407df1
zly <lang1326244830@qq.com>
2019-03-28 20:21:36 +0800

    测试

--- config/index.js
+++ config/index.js
@@ -53,7 +53,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: './bilibili-vue/',
+        assetsPublicPath: './',
 
         /**
          * Source Maps
