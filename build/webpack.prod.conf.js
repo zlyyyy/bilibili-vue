ef19b4f48e76842ee8a8cafb0266eec0ed75984d
zly <lang1326244830@qq.com>
2019-03-28 19:17:07 +0800

    测试

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -24,8 +24,8 @@ const webpackConfig = merge(baseWebpackConfig, {
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
     path: config.build.assetsRoot,
-    filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    filename: utils.assetsPath('js/[name]_[chunkhash].js'),
+    chunkFilename: utils.assetsPath('js/[name]_[chunkhash].js')
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
